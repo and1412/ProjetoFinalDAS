78ed4f1bd9b1bd9ec3f1c5c0a6d9357d4d58a1f0
kverrna <Ramon@192.168.1.6>
2013-02-25 23:36:55 -0300

    Teste de commint

---
+++ ProjetoFinalDAS/src/autenticacao/Autenticacao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autenticacao;
+
+/**
+ *
+ * @author Ramon
+ */
+public abstract class Autenticacao 
+{
+        public abstract boolean autenticar(String nome, String senha);
+	public abstract boolean cadastrar(String nome, String senha);
+	public abstract boolean alterar(String nome, String senha, String novoNome,String novaSenha);
+	public abstract boolean excluir(String nome, String senha);
+
+}
